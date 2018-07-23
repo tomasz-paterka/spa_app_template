31d62b79742b8cdd8c33826d17b81c83dacceb0a
Tomasz Paterka <tomekpaterka@gmail.com>
2018-07-23 22:57:11 +0200

    Add files structure, Sass to React, Home App components

---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import './Home.css';
+
+class Home extends Component {
+	render() {
+		return (
+			<div>
+				<div className="jumbotron">
+					<h1>MoGo</h1>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default Home;
